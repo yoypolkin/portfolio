54b7780f68f095f1a5b9c2cf4db2127852f106f4
YoYPolkin <polovynkoilya@gmail.com>
2025-10-15 20:19:34 +0200

    Remove unused import from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './globals.css';
 import NavLinks from '@/ui/nav-link';
-import Link from 'next/link';
 import { geistMono } from '@/ui/fonts';
 
 export default function RootLayout({
